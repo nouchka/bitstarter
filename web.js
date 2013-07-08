fff90947cdeaa4252ec8544dea533c0c9024b758
nouchka <git@katagena.com>
2013-07-08 22:39:01 +0000

    reading index.html

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-    response.send(data);
+    if (err) throw err;
+    response.send(data.toString());
   });
 });
 
